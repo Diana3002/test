1ee1b455f04f50386f1e6013ae7b99ad5cecf41e
parinova_diana <79607195+Diana3002@users.noreply.github.com>
2024-01-09 16:58:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const api_key = "3ea63741-5beb-47ef-b438-68d6ab706134";
 
 $.ajax(
     {
-        url: 'http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes?api_key=3ea63741-5beb-47ef-b438-68d6ab706134',
+        url: 'https://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes?api_key=3ea63741-5beb-47ef-b438-68d6ab706134',
         method: 'GET',
         datatype: "json",
         success: function(result){
